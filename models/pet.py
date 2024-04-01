8b64defad2b6ae139f156033e7350fde065cb1fc
austin <austinoblow97@gmail.com>
2024-04-01 13:32:33 -0500

    add instance_from_db / get_all / find_by_id / find_by_name methods to Pet

--- models/pet.py
+++ models/pet.py
@@ -74,3 +74,48 @@ class Pet:
         CURSOR.execute(sql, (self.id, ))
         CONN.commit()
 
+    @classmethod
+    def instance_from_db(cls, row):
+        pet = cls.all.get(row[0])
+        if pet:
+            pet.name = row[1]
+            pet.species = row[2]
+            pet.breed = row[3]
+            pet.age = row[4]
+            pet.price = row[5]
+        else:
+            pet = cls(row[1], row[2], row[3], row[4], row[5])
+            pet.id = row[0]
+            cls.all[pet.id] = pet
+        return pet
+    
+    @classmethod
+    def get_all(cls):
+        sql = '''
+            SELECT * FROM pets
+        '''
+
+        rows = CURSOR.execute(sql).fetchall()
+        return [cls.instance_from_db(row) for row in rows]
+    
+    @classmethod
+    def find_by_id(cls, id):
+        sql = '''
+            SELECT *
+            FROM pets
+            WHERE id = ?
+        '''
+
+        row = CURSOR.execute(sql, (id, )).fetchone()
+        return cls.instance_from_db(row) if row else None
+    
+    @classmethod
+    def find_by_name(cls, name):
+        sql = '''
+            SELECT *
+            FROM pets
+            WHERE name = ?
+        '''
+
+        row = CURSOR.execute(sql, (name, )).fetchone()
+        return cls.instance_from_db(row) if row else None
