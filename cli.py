d89343db85058344c48c5c1c8c2364380771d3dd
austin <austinoblow97@gmail.com>
2024-04-02 15:58:58 -0500

    add display_pet_management_menu

--- cli.py
+++ cli.py
@@ -9,6 +9,14 @@ class Main():
         print('2. Manage Pet Stores')
         print('3. Quit')
 
+    def display_pet_management_menu(self):
+        print('\nPet Management Menu')
+        print('1. Add a Pet')
+        print('2. View All Pets')
+        print('3. Update a Pet')
+        print('4. Delete a Pet')
+        print('5. Back to Main Menu')
+
 
 
 
