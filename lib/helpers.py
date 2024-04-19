db095958c296aadc64f732d9f13e411a28426b97
austin <austinoblow97@gmail.com>
2024-04-19 10:57:39 -0500

    update helpers.py to change from find_by_id to find_by_name / update seed.py

--- lib/helpers.py
+++ lib/helpers.py
@@ -12,8 +12,8 @@ def add_pet():
     name = input("Enter the pet's name: ")
     species = input("Enter the pet's species: ")
     breed = input("Enter the pet's breed: ")
-    age = input("Enter the pet's age: ")
-    price = input("Enter the pet's price: ")
+    age = int(input("Enter the pet's age: "))
+    price = int(input("Enter the pet's price: "))
     store_id = input("Enter the pet's store: ")
     try:
         pet = Pet.create(name, species, breed, age, price, store_id)
@@ -29,12 +29,12 @@ def display_pets():
 
 def update_pet():
     print('')
-    id_ = input("Enter the pet's id: ")
-    if pet := Pet.find_by_id(id_):
+    name = input("Enter the pet's name: ")
+    if pet := Pet.find_by_name(name):
         try:
             name = input("Enter the pet's new name: ")
             pet.name = name
-            price = input("Enter the pet's new price: ")
+            price = int(input("Enter the pet's new price: "))
             pet.price = price
             store_id = input("Enter the pet's new store: ")
             pet.store_id = store_id
@@ -44,16 +44,16 @@ def update_pet():
         except Exception as exc:
             print('Error updating pet: ', exc)
     else:
-        print(f'Pet {id_} not found')
+        print(f'Pet {name} not found')
 
 def delete_pet():
     print('')
-    id_ = input("Enter the pet's id: ")
-    if pet := Pet.find_by_id(id_):
+    name = input("Enter the pet's name: ")
+    if pet := Pet.find_by_name(name):
         pet.delete()
-        print(f'Pet {id_} deleted')
+        print(f'Pet {name} deleted')
     else:
-        print(f'Pet {id_} not found')
+        print(f'Pet {name} not found')
 
 # PetStore helper functions
         
@@ -76,8 +76,8 @@ def display_pet_stores():
 
 def update_pet_store():
     print('')
-    id_ = input("Enter the store's id: ")
-    if store := PetStore.find_by_id(id_):
+    name = input("Enter the store's name: ")
+    if store := PetStore.find_by_name(name):
         try:
             name = input("Enter the store's new name: ")
             store.name = name
@@ -89,13 +89,13 @@ def update_pet_store():
         except Exception as exc:
             print('Error updating store: ', exc)
     else:
-        print(f'Store {id_} not found')
+        print(f'Store {name} not found')
 
 def delete_pet_store():
     print('')
-    id_ = input("Enter the store's id: ")
-    if store := PetStore.find_by_id(id_):
+    name = input("Enter the store's name: ")
+    if store := PetStore.find_by_name(name):
         store.delete()
-        print(f'Store {id_} deleted')
+        print(f'Store {name} deleted')
     else:
-        print(f'Store {id_} not found')
+        print(f'Store {name} not found')
--- lib/seed.py
+++ lib/seed.py
@@ -1,16 +1,15 @@
 from models.pet_store import PetStore
 from models.pet import Pet
 
-def reset_database():
 
-    Pet.drop_table()
-    PetStore.drop_table()
-    
-    Pet.create_table()
-    PetStore.create_table()
+Pet.drop_table()
+PetStore.drop_table()
 
-reset_database()
+Pet.create_table()
+PetStore.create_table()
+
 petstore = PetStore.create('Petstore', 'Alton')
 ryleigh = Pet.create('Ryleigh', 'cat', 'black/white', 3, 150, petstore.id)
 lobster = Pet.create('Lobster', 'cat', 'orange', 2, 150, petstore.id)
 bean = Pet.create('Bean', 'cat', 'spotted', 1, 0, None)
+dobby = Pet.create('Dobby', 'dog', 'doberman pinscher', 1, 0, None)
